3d3e9c7b8ad42c1bdf781065a431bdb91c0217ed
Md Hasan Monsur <hmonsur56@gmail.com>
2023-07-08 13:22:13 +0600

    add second section

--- js/tab.js
+++ js/tab.js
@@ -14,3 +14,22 @@ tabs.forEach(tab => {
     target.classList.add('active')
   })
 })
+
+
+// Video tab
+const videoTabs = document.querySelectorAll('[data-tab-target]')
+const videoTabContent = document.querySelectorAll('[data-tab-content]')
+
+videoTabs.forEach(tabName => {
+  tabName.addEventListener('click', () => {
+    const target = document.querySelector(tabName.dataset.tabTarget)
+    videoTabContents.forEach(videoTabContent => {
+      videoTabContent.classList.remove('active')
+    })
+    videoTabs.forEach(tabName => {
+      tabName.classList.remove('active')
+    })
+    tabName.classList.add('active')
+    target.classList.add('active')
+  })
+})
